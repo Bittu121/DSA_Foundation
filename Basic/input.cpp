4eb0aefd96e2124f1fba184a2a855823c960ec00
Bittu <bittukumar8713@gmail.com>
2024-03-13 12:32:37 +0530

    commit

---
+++ Basic/input.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a, b;
+  cout << "Enter the numbers" << endl;
+  cin >> a >> b;
+  int c = a + b;
+  cout << c << endl;
+  return 0;
+}
