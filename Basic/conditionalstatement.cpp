6d4830b0c0d6d93172868a8d78069245020b9edd
Bittu <bittukumar8713@gmail.com>
2024-03-13 13:34:35 +0530

    commit

---
+++ Basic/conditionalstatement.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main()
+// {
+//   int a, b;
+//   cout << "Enter two numbers" << endl;
+//   cin >> a >> b;
+//   if (a == b)
+//   {
+//     cout << "Hey these are equal" << endl;
+//   }
+//   else
+//   {
+//     cout << "Not equal" << endl;
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   int a, b;
+//   cout << "Enter two numbers" << endl;
+//   cin >> a >> b;
+//   if (a == b)
+//   {
+//     cout << "Hey these are equal" << endl;
+//   }
+//   else
+//   {
+//     if (a < b)
+//     {
+//       cout << "a is samll" << endl;
+//     }
+//     else
+//     {
+//       cout << "a is greater" << endl;
+//     }
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   int a, b;
+//   cout << "Enter two numbers" << endl;
+//   cin >> a >> b;
+//   if (a == b)
+//   {
+//     cout << "Hey these are equal" << endl;
+//   }
+//   else if (a < b)
+//   {
+//     cout << "a is samll" << endl;
+//   }
+//   else
+//   {
+//     cout << "a is greater" << endl;
+//   }
+//   return 0;
+// }
+
+// check given num is even or odd
+// int main()
+// {
+//   int a;
+//   cout << "Enter numbers" << endl;
+//   cin >> a;
+//   if (a % 2 == 0)
+//   {
+//     cout << "Even" << endl;
+//   }
+//   else
+//   {
+//     cout << "odd" << endl;
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   int a = 10, b = 20;
+//   if (a <= b && !b) // b = 20 ,! b means non zero means false value
+//   {
+//     cout << "hello" << endl;
+//   }
+//   else
+//   {
+//     cout << "Hi";
+//   }
+//   return 0;
+// }
+
+// code
+int main()
+{
+  char c;
+  cin >> c;
+
+  if (c >= 65 && c <= 90)
+  {
+    cout << "1" << endl;
+  }
+  else if (c >= 97 && c <= 122)
+  {
+    cout << "0" << endl;
+  }
+  else
+  {
+    cout << "-1" << endl;
+  }
+  return 0;
+}
