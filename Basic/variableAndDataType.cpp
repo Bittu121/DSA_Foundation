d24645ae92abe08360d7f45538af32652c4e5447
Bittu <bittukumar8713@gmail.com>
2024-03-13 12:27:42 +0530

    commit

--- Basic/variableAndDataType.cpp
+++ Basic/variableAndDataType.cpp
@@ -2,36 +2,42 @@
 using namespace std;
 int main()
 {
-  int a = 10;
-  int b = 15;
-  int c = a + b;
-  cout << c << "integer size " << sizeof(c) << endl;
-  cout << "a" << endl;
+  // int a = 10;
+  // int b = 15;
+  // int c = a + b;
+  // cout << c << "integer size " << sizeof(c) << endl;
+  // cout << "a" << endl;
 
-  char d = 'x';
-  cout << d << " char size " << sizeof(d) << endl;
+  // char d = 'x';
+  // cout << d << " char size " << sizeof(d) << endl;
 
-  float f = 1.23;
-  cout << f << " float size " << sizeof(f) << endl;
+  // float f = 1.23;
+  // cout << f << " float size " << sizeof(f) << endl;
 
-  int newValue = f;
-  cout << newValue << endl;
+  // int newValue = f;
+  // cout << newValue << endl;
 
-  int n1 = 1234567891;
-  long long n2 = 5487956482;
-  long long sumOf = n1 + n2;
-  cout << sumOf << endl;
+  // int n1 = 1234567891;
+  // long long n2 = 5487956482;
+  // long long sumOf = n1 + n2;
+  // cout << sumOf << endl;
 
-  bool b1 = true;
-  cout << b1 << " boolean Size " << sizeof(b1) << endl;
+  // bool b1 = true;
+  // cout << b1 << " boolean Size " << sizeof(b1) << endl;
 
-  double d1 = 4.35;
-  cout << d1 << " double Size " << sizeof(d1) << endl;
+  // double d1 = 4.35;
+  // cout << d1 << " double Size " << sizeof(d1) << endl;
 
-  string str = "coding Ninja";
-  cout << str << " string size " << sizeof(str) << endl;
-  string str1 = " ";
-  cout << str1 << " string size " << sizeof(str1) << endl;
+  // string str = "coding Ninja";
+  // cout << str << " string size " << sizeof(str) << endl;
+  // string str1 = " ";
+  // cout << str1 << " string size " << sizeof(str1) << endl;
+
+  // calculate simple interest
+  float principle, rate, time;
+  cin >> principle >> rate >> time;
+  double si = (principle * rate * time) / 100;
+  cout << "simple interest " << si << endl;
   return 0;
 }
 
