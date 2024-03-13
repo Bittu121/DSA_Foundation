77da8687a5bf7528706b6ec7140c9bf6cb245f26
Bittu <bittukumar8713@gmail.com>
2024-03-13 15:46:45 +0530

    commit

--- Basic/whileLoop.cpp
+++ Basic/whileLoop.cpp
@@ -55,20 +55,143 @@ using namespace std;
 //   return 0;
 // }
 
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int i = 0, sum = 0;
+//   while (i <= n)
+//   {
+//     sum = sum + i;
+//     i = i + 2;
+//   }
+//   cout << sum << endl;
+//   return 0;
+// }
+// // Note-
+
+// // Tc O(n)
+// // Sc O(1)
+
+// pattern
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int i = 1;
+//   while (i <= n)
+//   {
+//     int j = 1;
+//     while (j <= i)
+//     {
+//       cout << j << " ";
+//       j = j + 1;
+//     }
+//     cout << endl;
+//     i = i + 1;
+//   }
+// }
+
+// int main()
+// {
+//   int rows;
+//   cout << "Enter the number of rows: ";
+//   cin >> rows;
+
+//   for (int i = 1; i <= rows; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << j << " ";
+//     }
+//     cout << endl;
+//   }
+
+//   return 0;
+// }
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int val = 1;
+//   int i = 1;
+//   while (i <= n)
+//   {
+//     int j = 1;
+//     while (j <= i)
+//     {
+//       cout << val << " ";
+//       j = j + 1;
+//       val = val + 1;
+//     }
+//     cout << endl;
+//     i = i + 1;
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   int n;
+//   cout << "Enter the number of rows: ";
+//   cin >> n;
+
+//   int count = 1;
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << count << " ";
+//       count++;
+//     }
+//     cout << endl;
+//   }
+//   return 0;
+// }
+
+//        1
+//      2 3
+//    4 5 6
+//  7 8 9 10
+
 int main()
 {
   int n;
   cin >> n;
-  int i = 0, sum = 0;
+
+  int val = 1;
+  int i = 1;
   while (i <= n)
   {
-    sum = sum + i;
-    i = i + 2;
+    int k = 1;
+    while (k <= n - i)
+    {
+
+      cout << "   ";
+      k = k + 1;
+    }
+    int j = 1;
+    while (j <= i)
+    {
+      cout << setw(2) << val << " "; // Use setw to align numbers
+      val = val + 1;
+      j = j + 1;
+    }
+    cout << endl;
+    i = i + 1;
   }
-  cout << sum << endl;
   return 0;
 }
-// Note-
-
-// Tc O(n)
-// Sc O(1)
