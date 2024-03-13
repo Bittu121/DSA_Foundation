d9e27f055af12bbbbb150c6eb203d5b693c37a2f
Bittu <bittukumar8713@gmail.com>
2024-03-13 11:49:10 +0530

    commit

---
+++ Basic/variableAndDataType.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout << "Hello\n";
+  cout << "world" << endl;
+  cout << "world!" << endl;
+  cout << "carrier"<< "labs";
+  return 0;
+}
