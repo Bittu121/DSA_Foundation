6d1549c7add5722d6573cfd47d049345e5a64e69
Bittu <bittukumar8713@gmail.com>
2024-03-13 12:57:35 +0530

    commit

---
+++ Basic/HowDataStored.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main()
+// {
+//   int a;
+//   char c;
+//   a = 19920;
+//   c = a;
+//   cout << c << endl;
+//   c = 'Y';
+//   a = c;
+//   cout << a << endl;
+//   float f = 1.23;
+//   a = f;
+//   cout << a << endl;
+//   return 0;
+// }
+
+// int main()
+// {
+//   int i = 10;
+//   char c = 'a';
+//   int output = i + c;
+//   cout << output << endl;
+//   char d = i + c;
+//   cout << d << endl;
+//   return 0;
+// }
+
+int main()
+{
+  char a = 234354;
+  cout << a << endl;
+  unsigned b = -123;
+  cout << b << endl;
+  return 0;
+}
