81bb717beb881a465069bc990443de494c548301
Bittu <bittukumar8713@gmail.com>
2024-03-19 22:50:02 +0530

    commit

--- Basic/8_pattern1.cpp
+++ Basic/8_pattern1.cpp
@@ -44,3 +44,25 @@ using namespace std;
 //   return 0;
 // }
 
+// 1111
+// 2222
+// 3333
+// 4444
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int i = 1;
+//   while (i <= n)
+//   {
+//     int j = 1;
+//     while (j <= n)
+//     {
+//       cout << i;
+//       j++;
+//     }
+//     cout << endl;
+//     i++;
+//   }
+//   return 0;
+// }
