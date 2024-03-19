613f9f38464ddf7c2a7fae67e81d1301d28e8b62
Bittu <bittukumar8713@gmail.com>
2024-03-19 22:40:15 +0530

    commit

---
+++ Basic/8_pattern1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// ****
+// ****
+// ****
+// ****
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   int i = 1;
+//   while (i <= n)
+//   {
+//     int j = 1;
+//     while (j <= n)
+//     {
+//       cout << "*";
+//       j++;
+//     }
+//     cout << endl;
+//     i++;
+//   }
+//   return 0;
+// }
+
+// 4444
+// 4444
+// 4444
+// 4444
+// int main(){
+//   int n;
+//   cin>>n;
+//   int i=1;
+//   while(i<=n){
+// 	  int j=1;
+// 	  while(j<=n){
+// 		  cout<<n;
+// 		  j++;
+// 	  }
+// 	  cout<<endl;
+// 	  i++;
+//   }
+//   return 0;
+// }
+
