6d447de31dfa5beb195e37ed273c01d3648a2d08
Bittu <bittukumar8713@gmail.com>
2024-04-07 21:42:37 +0530

    commit

--- Array/basic.cpp
+++ Array/basic.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+void printArray(){
+   
+}
 int main()
 {
   int number[15];
   cout<<endl<<"Everything is fine"<<endl<<endl;
   cout<<"value at 15 index "<<number[15]<<endl;
+
+  int second[3]={5,7,11};
+  cout<<"value at second index is :"<<second[2]<<endl;
+
+  int third[15]={2,7};
+  int n = 15;
+  for(int i=0;i<n;i++){
+    cout<<third[i]<<" ";
+  }
+
+  int n1 = 10;
+  int fourth[10]={0};
+  for(int i=0;i<n1;i++){
+    cout<<fourth[i]<<" ";
+  }
+
   return 0;
-}
+} 
