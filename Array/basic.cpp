2d7d3c898e2355716ae9c39db1d0a4e54aa7648a
Bittu <bittukumar8713@gmail.com>
2024-04-30 22:05:57 +0530

    first commit

--- Array/basic.cpp
+++ Array/basic.cpp
@@ -1,28 +1,85 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void printArray(){
-   
-}
-int main()
-{
-  int number[15];
-  cout<<endl<<"Everything is fine"<<endl<<endl;
-  cout<<"value at 15 index "<<number[15]<<endl;
+int main(){
+  // int b[10];
+  // b[0] = 1;
+  // b[1] = 10;
+  // cout<<b[0]<<endl;
+  // cout<<b[1]<<endl;
+  // cout<<b[2]<<endl;
+  // cout<<b[11]<<endl;
+  // cout<<"size of"<<sizeof(b);
 
-  int second[3]={5,7,11};
-  cout<<"value at second index is :"<<second[2]<<endl;
-
-  int third[15]={2,7};
-  int n = 15;
+  //Take input from user
+  int input[100];
+  int n;
+  cin>>n;
   for(int i=0;i<n;i++){
-    cout<<third[i]<<" ";
+    cin>>input[i];
   }
-
-  int n1 = 10;
-  int fourth[10]={0};
-  for(int i=0;i<n1;i++){
-    cout<<fourth[i]<<" ";
-  }
-
   return 0;
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void printArray(int arr[],int size){
+//    for (int i = 0; i < size; i++)
+//    {
+//     cout<<arr[i]<<" ";
+//    }
+   
+// }
+// int main()
+// {
+
+  // int number[15];
+  // cout<<endl<<"Everything is fine"<<endl<<endl;
+  // cout<<"value at 15 index "<<number[15]<<endl;
+
+
+  // int second[3]={5,7,11};
+  // cout<<"value at second index is :"<<second[2]<<endl;
+
+  // int n = 15;
+  // int third[15]={2,7};
+  // for(int i=0;i<n;i++){
+  //   cout<<third[i]<<" ";
+  // }
+  // printArray(third,n);
+
+  // int n = 10;
+  // int fourth[10]={0};
+  // for(int i=0;i<n;i++){
+  //   cout<<fourth[i]<<" ";
+  // }
+
+  // int n = 10;
+  // int fiveth[10]={1};
+  // for(int i=0;i<n;i++){
+  //   cout<<fiveth[i]<<" ";
+  // }
+
+//   return 0;
+// } 
